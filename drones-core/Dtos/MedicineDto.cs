0c5d34830dc657933c390a8941bac06618a405a6
Gustavo Martinez de Murga <gustavo.murga1996@gmail.com>
2024-05-06 07:36:11 -0400

    Add MedicineDto

---
+++ drones-core/Dtos/MedicineDto.cs
@@ -0,0 +1,20 @@
+﻿using drones_core.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace drones_core.Dtos
+{
+    public class MedicineDto
+    {
+        public string Guid { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public decimal Weight { get; set; }
+        public string Code { get; set; } = null!;
+        public string Image { get; set; } = null!;
+    }
+}
